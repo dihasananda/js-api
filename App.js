29619acf8485d3777fee87a526ca948d2732c304
dihasananda <dihasananda@gmail.com>
2022-06-17 08:39:55 +0700

    testing

---
+++ App.js
@@ -0,0 +1,20 @@
+const content = document.getElementById("content");
+
+fetch('https://fakestoreapi.com/products')
+            .then(res=>{
+                return res.json();
+            })
+            .then(json=>{
+                // json.map((item, index) => {
+                //     content.innerHTML=`<p>${item.title}</p>`;
+                //     console.log(item.title)
+                // })
+                // json.forEach(item => {
+                //     content.innerHTML=`<p>${item.title}</p>`;
+                //     console.log(item.title);
+                // });
+            })
+
+! function() {
+    console.log("hello");
+}
