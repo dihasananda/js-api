7d0fd96f21164a270c91eb0a56cb281a453122ea
dihasananda <dihasananda@gmail.com>
2022-06-20 21:31:07 +0700

    update App.js

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
 const content = document.getElementById("content");
 
-!async function() {
-    await fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json=>{
-                // json.map((item, index) => {
-                //     content.innerHTML=`<p>${item.title}</p>`;
-                //     console.log(item.title)
-                // })
-                // json.forEach(item => {
-                //     content.innerHTML=`<p>${item.title}</p>`;
-                //     console.log(item.title);
-                // });
-                for(let i = 0; i<json.length; i++){
-                    content.innerHTML=`<p>${json[i].title}</p>`;
-                    console.log(json[i].title);
-                }
-            })
-        }();
+fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(json => {
+        json.map((item, index) => {
+            content.innerHTML += `<p>${item.title}</p>`;
+            console.log(item.title)
+        })
+    })
