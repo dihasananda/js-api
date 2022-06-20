16f85df492474abb4b48472d656a850888735db5
Dihas Ananda <101080895+dihasananda@users.noreply.github.com>
2022-06-20 21:29:09 +0700

    Update App.js

--- App.js
+++ App.js
@@ -5,10 +5,10 @@ fetch('https://fakestoreapi.com/products')
                 return res.json();
             })
             .then(json=>{
-                // json.map((item, index) => {
-                //     content.innerHTML=`<p>${item.title}</p>`;
-                //     console.log(item.title)
-                // })
+                json.map((item, index) => {
+                    content.innerHTML += `<p>${item.title}</p>`;
+                    console.log(item.title)
+                })
                 // json.forEach(item => {
                 //     content.innerHTML=`<p>${item.title}</p>`;
                 //     console.log(item.title);
